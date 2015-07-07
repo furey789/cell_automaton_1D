57e44f30058478160771a0f751da2f937008934f
Peter Furey <prfurey@gmail.com>
2015-07-07 13:59:08 -0600

    Add seed number and state to model table and reach goal of a working user-chosen 1D cellular automaton

--- app/controllers/one_dim_rules_controller.rb
+++ app/controllers/one_dim_rules_controller.rb
@@ -14,12 +14,31 @@ class OneDimRulesController < ApplicationController
   def show
 
     @rule=One_dim_rule.find(params[:id])
-
     @arr_master=[]
     cols=95
     rows=225
     arr=Array.new(cols,0)
-    arr[(cols-1)/2]=1
+
+    if (@rule.seed_state == "Centered")
+      center_id = (cols-1)/2
+      start_id = center_id - (@rule.seed_number - 1)
+      (0..(@rule.seed_number-1)).each do |num|
+        id = start_id + (num * 2)
+        arr[id]=1
+      end
+    elsif (@rule.seed_state == "Random")
+      random_ids =[]
+      while random_ids.length < @rule.seed_number do
+        rand_num = rand(95)
+        if (random_ids.index(rand_num) == nil)
+          random_ids.push(rand_num)
+        end
+      end
+      random_ids.each do |id|
+        arr[id]=1
+      end
+    end
+
     @arr_master<<arr
 
     (0..rows).each do |i|
@@ -53,7 +72,7 @@ class OneDimRulesController < ApplicationController
   private
 
   def one_dim_rule_params
-    params.require(:one_dim_rule).permit(:t000,:t010,:t001,:t011,:t100,:t110,:t101,:t111)
+    params.require(:one_dim_rule).permit(:t000,:t010,:t001,:t011,:t100,:t110,:t101,:t111,:seed_number,:seed_state)
   end
 
 end
