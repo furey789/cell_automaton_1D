6a6693496b3f13662727ae40a3f2adc9e46ea20f
Peter Furey <prfurey@gmail.com>
2015-08-22 09:38:30 -0600

    Work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+One_dim_rule.create( id: 1, name: "initial", t000: 0, t010: 0, t001: 0, t011: 0, t100: 0, t110: 0, t101: 0, t111: 0, seed_number: 1, seed_state: "Centered")
+One_dim_rule.create( id: 999, name: "most_recent", t000: 0, t010: 0, t001: 1, t011: 0, t100: 0, t110: 0, t101: 0, t111: 0, seed_number: 1, seed_state: "Centered")
